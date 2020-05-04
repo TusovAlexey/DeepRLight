9cc00b95ce13db88e001aa719dd20643a5c8edd6
Alexey <tusovalexey@gmail.com>
2020-05-04 04:15:10 +0300

    - Derech akko small

--- Utils/ArgParser.py
+++ Utils/ArgParser.py
@@ -8,11 +8,11 @@ def process_arguments():
                                                  "\tAlexey Tusov, tusovalexey[at]gmail.com\n" \
                                                  "\tPavel Rastopchin, pavelr[at]gmail.com\n", \
                                                     epilog="")
-    parser.add_argument("-sc", "--sumo-cfg", type=str, default="./Networks/double/double.sumocfg", dest='cfg')
+    parser.add_argument("-sc", "--sumo-cfg", type=str, default="./Networks/Derech_akko_small/Derech_akko_small.sumocfg", dest='cfg')
     parser.add_argument("-gm", "--gui-mode", type=bool, default=False, dest='gui')
     parser.add_argument("-e", "--episodes", type=int, default=20, dest='episodes',
                         help='Number of episodes for simulation, default: agent\'s default value')
-    parser.add_argument("-s", "--steps", type=int, default=30000, dest='max_steps',
+    parser.add_argument("-s", "--steps", type=int, default=86400, dest='max_steps',
                         help='Number of steps')
     parser.add_argument("-a", "--animation", type=bool, default=False, dest='animation')
     return parser.parse_args()
